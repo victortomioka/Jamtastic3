967fa7fc5a7e505d5406584a9d2126c98861e70b
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-02-17 22:22:02 -0200

    Movimentação Básica
    - Script PlayerMovement com a movimentação básica do personagem;
    - Script LookAtCursor que faz com que o objeto olhe em direção ao
    cursor;
    - Criada camada "Ground" para representar o chão

---
+++ Assets/Scripts/LookAtCursor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCursor : MonoBehaviour 
+{
+	public Camera cam;
+	public LayerMask groundMask;
+
+	[HideInInspector] public Vector3 lookDirection;
+
+	private void Reset() 
+	{
+		cam = Camera.main;	
+		groundMask = LayerMask.GetMask("Ground");
+	}
+
+	private void Update() 
+	{
+		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, 100, groundMask))
+		{
+			Vector3 pos = transform.position;
+
+			lookDirection = (hit.point - pos).normalized;
+			lookDirection.y = 0;
+			transform.LookAt(pos + lookDirection, Vector3.up);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float speed;
+
+	private Rigidbody rb;
+
+	private void Start() 
+	{
+		rb = GetComponent<Rigidbody>();	
+	}
+
+	void FixedUpdate() 
+	{
+		float axisHorizontal = Input.GetAxis("Horizontal");
+		float axisVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(axisHorizontal, 0, axisVertical) * speed * Time.deltaTime;
+		Vector3 position = rb.position + movement;
+		rb.MovePosition(position);
+	}	
+}
