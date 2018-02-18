630cd848596c1dac5305edd4b68318b0453fdc31
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-02-18 17:37:15 -0300

    Ajustes no script de mira do jogador
    O Script que faz com que o objeto do jogador vire de acordo com a
    posição do mouse foi alterado para não precisar usar mais o chão da cena
    como referência.
    
    Essa mudança foi realizada para evitar futuros problemas em situações
    onde o chão não seja completamente reto, por exemplo, em vãos e buracos,
    ou partes do mapa que sejam mais altas do que outras. Como o script
    lançava um raycast partindo da câmera até o chão, isso poderia causar
    comportamentos estranhos. Por exemplo: ao mover o cursor em uma parte
    que não tenha chão, o personagem simplesmente parava de olhar para ele,
    ou em casos em que a parte do chão onde o cursor está apontando for mais
    alto que a parte onde o jogador está, ele não olhar para a direção
    correta.
    
    Para resolver isso, o script usa um Plane como referência para realizar
    o Raycast ao invés do chão. Assim o objeto sempre olha em direção ao
    cursor independente da posição do chão.
    
    A solução foi baseada nessa resposta:
    https://answers.unity.com/questions/932115/top-down-aim-at-mouse.html

--- Assets/Scripts/LookAtCursor.cs
+++ Assets/Scripts/LookAtCursor.cs
@@ -5,28 +5,33 @@ using UnityEngine;
 public class LookAtCursor : MonoBehaviour 
 {
 	public Camera cam;
-	public LayerMask groundMask;
 
 	[HideInInspector] public Vector3 lookDirection;
 
+	private Plane groundPlane;
+
+	private void Start()
+	{
+		groundPlane = new Plane(Vector3.up, Vector2.zero);	
+	}
+
 	private void Reset() 
 	{
-		cam = Camera.main;	
-		groundMask = LayerMask.GetMask("Ground");
+		cam = Camera.main;
 	}
 
 	private void Update() 
 	{
 		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
-		RaycastHit hit;
-		if(Physics.Raycast(ray, out hit, 100, groundMask))
+		float enter;
+		if(groundPlane.Raycast(ray, out enter))
 		{
-			Vector3 pos = transform.position;
-
-			lookDirection = (hit.point - pos).normalized;
+			Vector3 hitPoint = ray.GetPoint(enter);
+			Vector3 lookDirection = (hitPoint - transform.position).normalized;
 			lookDirection.y = 0;
-			transform.LookAt(pos + lookDirection, Vector3.up);
+
+			transform.LookAt(transform.position + lookDirection, Vector3.up);
 		}
 	}
 }
