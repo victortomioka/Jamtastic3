794231df2e12dc5db6dc6dddbb021a70698d3f21
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-02-20 11:15:14 -0300

    Mecânica de Esquiva (Dash)
    - Criado script "DashMovement" que controla o movimento de esquiva. Ele
    pode ser configurado com a distância da esquiva, a velocidade e o tempo
    de intervalo entre as esquivas (cooldown)
    - O script PlayerController foi ajustado para usar a mecânica de dash ao
    clicar com o botão direito;
    - Foi adicionada uma verificação em PlayerController para interromper o
    movimento de esquiva ao tocar em outros objetos, como paredes ou
    inimigos

---
+++ Assets/Scripts/DashMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DashMovement : MonoBehaviour
+{
+    public float distance;
+    public float speed;
+    public float cooldown;
+
+    public TrailRenderer trail;
+
+    private bool coolingDown;
+    private Rigidbody rb;
+
+    public bool IsDashing { get; private set; }
+    public bool IsDashAllowed { get{ return !IsDashing && !coolingDown; } }
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Dash(Vector3 direction)
+    {
+        if (IsDashAllowed)
+            StartCoroutine("DashCoroutine", direction);
+    }
+
+    public void Interrupt()
+    {
+        StopCoroutine("DashCoroutine");
+
+        IsDashing = false;
+        SendMessage("DashEnded", null, SendMessageOptions.DontRequireReceiver);
+
+        SetTrailEnabled(false);
+    }
+
+    private IEnumerator DashCoroutine(Vector3 direction)
+    {
+        IsDashing = true;
+        SendMessage("DashStarted", null, SendMessageOptions.DontRequireReceiver);
+
+        SetTrailEnabled(true);
+
+        float travelled = 0;
+        Vector3 startPosition = rb.position;
+
+        while(travelled <= distance)
+        {
+            yield return new WaitForEndOfFrame();
+
+            Vector3 movement = direction * speed * Time.deltaTime;
+            rb.MovePosition(rb.position + movement);
+
+            travelled = Vector3.Distance(startPosition, rb.position);
+        }
+
+        IsDashing = false;
+        SendMessage("DashEnded", null, SendMessageOptions.DontRequireReceiver);
+
+        SetTrailEnabled(false);
+
+        coolingDown = true;
+        yield return new WaitForSeconds(cooldown);
+        coolingDown = false;
+    }
+
+    private void SetTrailEnabled(bool enabled)
+    {
+        if(trail != null)
+            trail.enabled = enabled;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,21 @@ public class PlayerController : MonoBehaviour
 {
 	public PlayerMovement movement;
 	public Gun gun;
+    public DashMovement dashMovement;
+    
+    private int groundLayer;
 
-	private void Reset() 
+    private void Start() 
+    {
+        groundLayer = LayerMask.NameToLayer("Ground");
+    }
+
+    private void Reset() 
 	{
 		movement = GetComponent<PlayerMovement>();
 		gun = GetComponentInChildren<Gun>();
-	}
+        dashMovement = GetComponent<DashMovement>();
+    }
 
 	void FixedUpdate() 
 	{
@@ -24,12 +33,37 @@ public class PlayerController : MonoBehaviour
 
 	private void Update() 
 	{
-		if(Input.GetMouseButton(0) && IsEnabled(gun))
+        if (Input.GetMouseButton(0) && IsEnabled(gun))
 			gun.Shoot();
+
+        if (Input.GetMouseButtonDown(1) && IsEnabled(dashMovement))
+            dashMovement.Dash(transform.forward);
 	}
+
+    private void DashStarted()
+    {
+        movement.enabled = false;
+        gun.enabled = false;
+    }
+
+    private void DashEnded()
+    {
+        movement.enabled = true;
+        gun.enabled = true;
+    }
 
 	private bool IsEnabled(MonoBehaviour component)
 	{
 		return component != null && component.enabled;
 	}
+
+    private void OnCollisionStay(Collision other) 
+    {
+        if(other.gameObject.layer == groundLayer)
+            return;
+
+        if (dashMovement.IsDashing)
+            dashMovement.Interrupt();
+    }
+
 }
