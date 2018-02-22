2c501b7483e56cb8e6ce20757a1e2c9d1b2e1fc3
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-02-21 23:54:35 -0300

    Implementando inimigos
    - Implementando inimigo de ataque curto;
    - Criada cena de teste "Enemies" para testar as mecnicas dos inimigos.
    Nele foi criado um NavMesh para o inimigo seguir o jogador e conseguir
    desviar dos objetos

---
+++ Assets/Scripts/Characters/Enemies/EnemyMelee.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMelee : EnemyCharacter
+{
+    public float attackRange;
+    public float followRange;
+
+    private bool startedFollow;
+    private LayerMask playerMask;
+
+    private FollowTarget followTarget;
+    private Animator anim;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        playerMask = LayerMask.GetMask("Player");
+        followTarget = GetComponent<FollowTarget>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        Collider[] hits;
+
+        if (!startedFollow)
+        {
+            hits = Physics.OverlapSphere(transform.position, followRange, playerMask);
+            if (hits.Length > 0)
+            {
+                followTarget.StartFollowing();
+                startedFollow = false;
+            }
+        }
+
+        hits = Physics.OverlapSphere(transform.position, attackRange, playerMask);
+        if(hits.Length > 0)
+        {
+            AttackStart();
+        }
+
+        anim.SetBool("walking", followTarget.following);
+    }
+
+    public void AttackStart()
+    {
+        anim.SetTrigger("attack");
+        followTarget.StopFollowing();
+    }
+
+    public void AttackEnd()
+    {
+        followTarget.StartFollowing();
+    }
+
+    public override void TakeHit(float damage)
+    {
+        anim.SetTrigger("getHit");
+
+        base.TakeHit(damage);
+    }
+
+    protected override void Die()
+    {
+        anim.SetTrigger("die");
+
+        followTarget.StopFollowing();
+        
+        //base.Die();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+
+        Gizmos.color = new Color(255, 153, 0);
+        Gizmos.DrawWireSphere(transform.position, followRange);
+    }
+}
---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+
+    [HideInInspector] public bool following;
+
+    private NavMeshAgent navAgent;
+
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (!following)
+            return;
+
+        Vector3 targetPos = target.position;
+        targetPos.y = transform.position.y;
+
+        navAgent.SetDestination(targetPos);
+    }
+
+    public void SetSpeed(float newSpeed)
+    {
+        navAgent.speed = newSpeed;
+    }
+
+    public void StartFollowing()
+    {
+        following = true;
+    }
+
+    public void StopFollowing()
+    {
+        following = false;
+    }
+}
