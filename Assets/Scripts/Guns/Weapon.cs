a72ad299e975bd607097a9b98ef98b8509291fcd
LeandroDotta <leandro.dotta@yahoo.com.br>
2018-02-22 23:06:46 -0300

    Revert "Revert "Scriptable Object das armas""
    This reverts commit 98762a911fd36c5afdff0b343de44894cf78869d.

---
+++ Assets/Scripts/Guns/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Weapon", menuName = "Weapons")]
+public class Weapon : ScriptableObject {
+    public enum WeaponClass {Shotgun, Pistol}
+    public WeaponClass Class;
+    public enum WeaponCategory {PrimaryWeapon, SecondaryWeapon}
+    public WeaponCategory Category = WeaponCategory.PrimaryWeapon;
+
+    public float Damage;
+    public float FireRate;
+    public float BulletSpeed;
+
+    public float ShotgunBulletsCount;
+    public float ShotgunSpread;
+
+    public new string name;
+
+    public GameObject BulletPrefab;
+
+   
+	
+}
